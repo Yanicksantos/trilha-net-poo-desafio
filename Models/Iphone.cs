ccafaadf9e43a597a96f1a8ea63595db889fe6a1
Yanick Santos <yanickdev@outlook.com>
2023-12-24 09:02:24 -0300

    ajuste classe iphone

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -5,7 +5,7 @@ namespace DesafioPOO.Models
         
         public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
-           
+            
         }
 
         public override void InstalarAplicativo(string nomeApp)
